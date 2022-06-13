6cd9eb4f105e36e5f421eabe8ea62337e833e4f7
Justin Teats <justinteats@Justins-MacBook-Air.local>
2022-06-12 20:07:18 -0700

    added team setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer =require("inquirer");
+
+const fs =require("fs");
+const jest = require("jest");
+
+const Manager = require('./__tests__/Manager.test');
+const Engineer = require('./__tests__/Engineer.test');
+const Intern = require('./__tests__/Intern.test');
+const Employee = require('./__tests__/Employee.test');
+
